e95a80a9caf4eaf8f70a85760c7832c415a05152
unknown <antonio.girsang@gmail.com>
2025-11-22 08:13:13 +0700

    fix: remove unsupported vite-plugin-laravel-blade

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import blade from 'vite-plugin-laravel-blade';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: false,
-        }),
         blade({
             // Ini yang bikin @extends & @yield jalan tanpa PHP!
             input: 'resources/views/welcome.blade.php',
